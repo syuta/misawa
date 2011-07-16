f7beb78d6db7b8bf6c10fbe8ee1f1ee6c27fa899
syuta <nakamura.shuta@gmail.com>
2011-07-16 10:21:17 +0900

    サイズ調整

--- misawa.js
+++ misawa.js
@@ -11,17 +11,20 @@ req.onreadystatechange = function() {
 		//最新のentry_areaを取得
 		var text1 = html.substring(html.indexOf('<div class="entry_area">'));
 		var text2 = text1.substring(0,text1.indexOf('</div>'));
+
 		//タイトル取り出し
 		var titleStart = text2.substring(text1.indexOf('<h2>') + h2Size);
 		var title = titleStart.substring(0,titleStart.indexOf('</h2>'));
 		var horesase_title = document.getElementById("horesase_title");
-		horesase_title.innerHTML = "<font size=3>"+ title +"</font>";
+		horesase_title.innerHTML = "<font size=2>"+ title +"</font>";
 
 		//画像URL取り出し
 		var imageStart = text2.substring(text2.indexOf('src="') + srcSize);
 		var imageUrl = imageStart.substring(0,imageStart.indexOf('"')); 
 		var horesase_image = document.getElementById("horesase_image");
 		horesase_image.src = imageUrl;
+		horesase_image.height = 320;
+		horesase_image.width = 240;
 	}
 };
 
